5cd38f2c7a4efba4f9c2c567736ffbe0db684299
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-06-08 09:56:12 +0600

    make a  admin successfully

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,9 +1,10 @@
 import { FaCalendarAlt, FaHome, FaUsers } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin";
 
 
 const DashBoard = () => {
-    const isAdmin =true;
+    const [isAdmin] = useAdmin();
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
